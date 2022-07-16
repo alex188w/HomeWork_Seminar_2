00c063057344c1f79f00cb4f4a7cce2bb7f55b12
Алексей Шевченко <alex18w@mail.ru>
2022-07-16 09:50:24 +0500

    Залача 10

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число и нажмите клавишу Enter: ");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+int tens = number / 10 % 10;
+
+Console.WriteLine("Вторая цифра этого числа: " + tens);
