ba443b5d90325cb03ee3c747e322ff6f43de0e8a
Алексей Шевченко <alex18w@mail.ru>
2022-07-16 10:47:00 +0500

    Задача 13

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите заданное число и нажмите клавишу Enter: ");
+int number =  Convert.ToInt32(Console.ReadLine());
+int numberThree3 = number % 10;
+int numberThree4 = number / 10 % 10;
+int numberThree5 = number / 100 % 10;
+int numberThree6 = number / 1000 % 10;
+
+if (number < 100) Console.WriteLine("В этом числе нет третьей цифры");
+else 
+    {
+        if (number > 99 && number < 1000)
+        Console.WriteLine("Третья цифра заданного чмсла: " + numberThree3);
+        else
+            if (number > 999 && number < 10000)
+            Console.WriteLine("Третья цифра заданного чмсла: " + numberThree4);
+            else
+                if (number > 9999 && number < 100000)
+                Console.WriteLine("Третья цифра заданного чмсла: " + numberThree5);
+                else
+                    if (number > 99999 && number < 1000000)
+                    Console.WriteLine("Третья цифра заданного чмсла: " + numberThree6);
+    }
+// и т.д. при каждом увеличении разряда добавляем новую переменную numberThree и новое условие
