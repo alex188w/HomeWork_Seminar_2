61ef32e6decc8f5a35d221a693f639e911823e65
Алексей Шевченко <alex18w@mail.ru>
2022-07-17 10:08:24 +0500

    add Решение Задачи 13 с помощью массива  и метода

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -1,13 +1,32 @@
-﻿Console.Write("Введите заданное число и нажмите клавишу Enter: ");
-int number =  Convert.ToInt32(Console.ReadLine());
+﻿//Задача 13: Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-if (number < 100) Console.WriteLine("В этом числе нет третьей цифры");
-else
+void Zadacha_13() //Решение с помощью цикла 
 {
-    while (number > 999)
+    Console.Write("Введите заданное число и нажмите клавишу Enter: ");
+    int number =  Convert.ToInt32(Console.ReadLine());
+    number = Math.Abs(number); // присвоение модуля числа
+    if (number < 100) Console.WriteLine("В этом числе нет третьей цифры");
+    else
     {
-        number /= 10; // знак "/ =" обозначает: number = number / 10
+        while (number > 999)
+        {
+            number /= 10; // знак "/ =" обозначает: number = number / 10
+        }
+        Console.WriteLine("Третья цифра заданного чмсла: " + number % 10);
     }
-    Console.WriteLine("Третья цифра заданного чмсла: " + number % 10);
 }
-                
+//Zadacha_13();    
+
+void Zadacha_13_1() //Решение с помощью массива
+{
+    Console.Write("Введите заданное число и нажмите клавишу Enter: ");
+    string number =  Console.ReadLine();
+    if (number.Length > 2)
+    {
+        Console.WriteLine("Третья цифра заданного чмсла: " + number[2]);
+    }
+    else
+    Console.WriteLine("В этом числе нет третьей цифры"); 
+}
+
+Zadacha_13_1();
